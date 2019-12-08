cccb4552a4408692d95d30a7ea08f7cc16c10028
Seiya Nuta <nuta@seiya.me>
2019-12-08 15:32:56 +0900

    Add resea

--- build.sh
+++ build.sh
@@ -3,15 +3,17 @@ rm -rf public
 mkdir -p public
 
 cp *.html *.css *.jpg public
+cp -r resea public
 
+mkdir -p repos
 #
 #  resea
 #
-if [[ ! -d resea ]]; then
-    git clone https://github.com/seiyanuta/resea
+if [[ ! -d repos/resea ]]; then
+    git clone https://github.com/seiyanuta/resea repos/resea
 fi
-cd resea
+pushd repos/resea
 git pull
 pip3 install -r tools/requirements.txt
 make docs
-cp -r build/docs ../public/resea
+cp -r build/docs ../../public/resea/docs

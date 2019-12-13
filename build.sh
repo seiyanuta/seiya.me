e936c2d150b48ea46f0044ec6ecd000ef4a3ce4a
Seiya Nuta <nuta@seiya.me>
2019-12-13 10:28:41 +0900

    Add post.scss

--- build.sh
+++ build.sh
@@ -3,8 +3,31 @@ rm -rf public
 mkdir -p public
 
 cp *.html *.css *.jpg public
+cp -r images public
 cp -r resea public
 cp -r nsh public
+
+#
+#  Posts
+#
+mkdir -p build
+sass post.scss build/post.css
+for adoc in *.adoc; do
+    asciidoctor -a stylesheet=build/post.css -o "public/${adoc%.*}.html" "$adoc"
+    sed -i .bak "s#</head># \
+        <script async src=\"https://www.googletagmanager.com/gtag/js?id=UA-145819933-1\"></script> \
+        <script> \
+          window.dataLayer = window.dataLayer || []; \
+          function gtag(){dataLayer.push(arguments);} \
+          gtag('js', new Date()); \
+          gtag('config', 'UA-145819933-1'); \
+        </script></head>#" "public/${adoc%.*}.html"
+    rm "public/${adoc%.*}.html.bak"
+done
+
+if [[ "$POSTS_ONLY" != "" ]]; then
+  exit 0
+fi
 
 mkdir -p repos
 #
